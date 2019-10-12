43262295231e13440c3a4666a986c09357d4661c
alexrMEI <2190273@my.ipleiria.pt>
2019-10-12 11:29:29 +0100

    database max length fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 191);
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('provider')->nullable();
-            $table->string('provider_id')->nullable();
+            $table->string('password', 191);
+            $table->string('provider', 191)->nullable();
+            $table->string('provider_id', 191)->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
