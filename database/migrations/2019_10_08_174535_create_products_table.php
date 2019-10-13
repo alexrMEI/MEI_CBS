fd2e6a7cd62658b5160863dbaaac386099bee8f8
alexrMEI <2190273@my.ipleiria.pt>
2019-10-13 19:16:16 +0100

    migrations fix

--- database/migrations/2019_10_08_174535_create_products_table.php
+++ database/migrations/2019_10_08_174535_create_products_table.php
@@ -15,7 +15,7 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 255);
+            $table->string('name', 191);
             $table->double('price', 10, 2);
             $table->longText('description');
             $table->timestamps();
--- database/migrations/2019_10_08_184820_create_product_files_table.php
+++ database/migrations/2019_10_08_184820_create_product_files_table.php
@@ -16,9 +16,9 @@ class CreateProductFilesTable extends Migration
         Schema::create('product_files', function (Blueprint $table) {
             $table->bigIncrements('id');
 
-            $table->string('name')->unique();
-            $table->string('file_path')->unique();
-            $table->string('product_version');
+            $table->string('name', 191)->unique();
+            $table->string('file_path', 191)->unique();
+            $table->string('product_version', 191);
 
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
