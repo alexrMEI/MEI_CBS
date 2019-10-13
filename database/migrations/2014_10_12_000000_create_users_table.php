ccf8a68cffa3b8249da8a88757380f1e7465ccf2
alexrMEI <2190273@my.ipleiria.pt>
2019-10-13 13:47:21 +0100

    update users table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name', 191);
             $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password', 191);
+            $table->string('password', 191)->nullable();
             $table->string('provider', 191)->nullable();
             $table->string('provider_id', 191)->nullable();
             $table->rememberToken();
